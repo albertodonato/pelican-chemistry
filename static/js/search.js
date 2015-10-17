7b6cdf11412a6dc78dfaa7e96361573cf8b392f6
Alberto Donato <alberto.donato@gmail.com>
2015-10-17 05:33:58 +0300

    Add search form.

---
+++ static/js/search.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    var icon = $('#search-icon'),
+        input = $('#tipue_search_input');
+    
+    input.keyup(function(e) {
+        if(e.keyCode == 27){
+            input.blur();
+        }
+    });
+    icon.click(function(e) {
+        e.preventDefault();
+        icon.hide();
+        input.show().focus();
+    });
+    input.blur(function() {
+        input.hide();
+        icon.show();
+    });
+});
